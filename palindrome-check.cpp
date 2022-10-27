c405880ceb3bd6b08c3c664633c6521a97b6a2c1
sunandan <sunandanlalka@gmail.com>
2022-10-27 22:12:01 +0530

    palindrome check

---
+++ palindrome-check.cpp
@@ -0,0 +1,18 @@
+// palindrome check
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    char string[n + 1];
+
+    for (int i = 0; i < n; i++){
+        if(string[i] != n-1-i){
+            
+        }
+    }
+
+        return 0;
+}
