0cb23a030e5ba3d45e8453bcc563e00945a00888
sunandan <sunandanlalka@gmail.com>
2022-10-28 00:00:04 +0530

    done

--- palindrome-check.cpp
+++ palindrome-check.cpp
@@ -8,11 +8,19 @@ int main(){
     cin >> n;
     char string[n + 1];
 
+    bool flag = true;
+
     for (int i = 0; i < n; i++){
         if(string[i] != n-1-i){
-            
+            flag = false;
         }
     }
+
+if(flag == true){
+    cout << "palindrome";
+}else{
+    cout << "not palindrome";
+}
 
         return 0;
 }
