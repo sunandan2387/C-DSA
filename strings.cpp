cd63c1de32ac3a01a80a09b73ac83e32fdbcf176
sunandan <sunandanlalka@gmail.com>
2022-10-30 20:47:18 +0530

    string declaration

--- strings.cpp
+++ strings.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 int main(){
-
+// by input
     string str;
     cin >> str;
-    cout << str;
+    cout << str<<endl;
+//by declaration
+    string str1 = "sunandan";
+    cout << str1<<endl;
+//repetitive character
+    string str2(5, 'a');
+    cout << str2 << endl;
 
     return 0;
 }
