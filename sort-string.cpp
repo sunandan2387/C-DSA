aa71f3e9859b78b0f6b854edd4ea1e9b8445f951
sunandan <sunandanlalka@gmail.com>
2022-10-30 21:38:52 +0530

    sort string

---
+++ sort-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string s1 = "dshfkagdssikfgbawe";
+    sort(s1.begin(), s1.end());
+    cout << s1 << endl;
+
+    return 0;
+}
