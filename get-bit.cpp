2d6d1bbc4e86e86bcfc45c1d95d727055f82b254
sunandan <sunandanlalka@gmail.com>
2022-11-03 21:32:03 +0530

    words in string

--- get-bit.cpp
+++ get-bit.cpp
@@ -1,8 +1,34 @@
 #include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
 
 main(){
-    int n = 5;
 
-    cout << n*2 << endl;
+   string wishlist;
+   getline(cin,wishlist);
+   cin.ignore();
+   string item;
+   getline(cin,item);
+
+
+   int start =0, flag =0;
+   string word;
+
+   for (int i = 0; i < wishlist.length()+1;i++){
+    if( (wishlist[i] == ' ') || (wishlist[i] == '\0')){
+        word = wishlist.substr(start, i);
+        if(item.compare(word) == 0){
+            flag = 1;
+        }
+        start = i + 2;
+    } 
+   }
+if(flag ==1){
+    cout << "yoink" << endl;
+}else{
+    cout << "yeet" << endl;
+}
+     
+
+   return 0;
 }
