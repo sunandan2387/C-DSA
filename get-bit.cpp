6cabf677c762ab06a7b7bb7c09084bcc0629e13d
sunandan <sunandanlalka@gmail.com>
2022-11-13 20:59:07 +0530

    clear bit in bit manipulation

--- get-bit.cpp
+++ get-bit.cpp
@@ -10,9 +10,14 @@ int setBit(int n, int pos){
   return (n | (1 << pos));
 }
 
+int clearBit(int n, int pos){
+  int mask = ~(1 << pos);
+  return (n & mask);
+}
+
 main(){
 
-  cout << setBit(5, 1) << endl;
+  cout << clearBit(5, 2) << endl;
 
   return 0;
 }
