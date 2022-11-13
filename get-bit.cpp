d62b714c07dbdf86eee737c825977c0200c4d55f
sunandan <sunandanlalka@gmail.com>
2022-11-13 21:02:48 +0530

    update bit in bit manipulation

--- get-bit.cpp
+++ get-bit.cpp
@@ -15,9 +15,15 @@ int clearBit(int n, int pos){
   return (n & mask);
 }
 
+int updateBit(int n, int pos, int value){
+  int mask = ~(1 << pos);
+  n = n & mask;
+  return (n | (value << pos));
+}
+
 main(){
 
-  cout << clearBit(5, 2) << endl;
+  cout << updateBit(5,1,1) << endl;
 
   return 0;
 }
