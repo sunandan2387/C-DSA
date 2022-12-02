3ced71b2b2af57da15f24ec53b015055de160754
sunandan <sunandanlalka@gmail.com>
2022-12-02 14:25:53 +0530

    practice

--- testingbinary.cpp
+++ testingbinary.cpp
@@ -3,20 +3,30 @@
 using namespace std;
 
 int main(){
-    int array[]={2,4,1,5,7,3,6,4};
-    int n = end(array) - begin(array);
+    int n, check;
 
-    for (int i = 0; i < n;i++){
-        int temp;
-        if(array[i+1]<array[i]){
-            array[i] = temp;
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
-        }
-    }
-        for (int i = 0; i < n;i++){
-            cout << array[i];
-        }
+    cin >> n >> check;
+    int arr[n];
+    cout << " enter array " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+  }
+  int start = 0;
+  int end = n;
 
-        return 0;
+  for (int i = 0; i < n;i++)
+  {
+      int mid = (start + end) / 2;
+      if (arr[mid] == check){
+          cout << "found at " << mid << endl;
+          break;
+      }else if(arr[mid]<check){
+          start = mid+1;
+      }else{
+          end = mid-1;
+      }
+  }
+
+      return 0;
 }
